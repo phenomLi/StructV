07c83f01dcd4a14c36942d580db8b74e4effca60
phenomLi <1543046129@qq.com>
2020-04-03 23:26:07 +0800

    删除@types

--- @types/Interaction/zoom.d.ts
+++
@@ -1,8 +0,0 @@
-import { Interaction } from "./interaction";
-export declare class Zoom extends Interaction {
-    zoomValue: number;
-    maxZoomValue: number;
-    minZoomValue: number;
-    constructor();
-    zoom(): void;
-}
